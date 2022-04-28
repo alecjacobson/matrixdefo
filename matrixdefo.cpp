d3f87d80f36caf9c1009204cab103bb2bfb1cc8a
Alec Jacobson <alecjacobson@adobe.com>
2022-04-27 21:39:11 -0400

    libigl → v2.4.0

--- matrixdefo.cpp
+++ matrixdefo.cpp
@@ -1,10 +1,13 @@
 
 #include <igl/read_triangle_mesh.h>
-#include <igl/viewer/Viewer.h>
+#include <igl/opengl/glfw/Viewer.h>
+#include <igl/opengl/report_gl_error.h>
+#include <igl/opengl/create_shader_program.h>
+#include <igl/opengl/destroy_shader_program.h>
 
 #include <igl/readDMAT.h>
 #include <igl/LinSpaced.h>
-#include <igl/opengl/report_gl_error.h>
+#include <igl/opengl/destroy_shader_program.h>
 
 #include <Eigen/Core>
 
@@ -22,7 +25,11 @@ int main(int argc, char *argv[])
   Eigen::Matrix< float,Eigen::Dynamic,1> I;
   Eigen::Matrix< float,Eigen::Dynamic,3,Eigen::RowMajor> tex;
   Eigen::Matrix< float,Eigen::Dynamic,Eigen::Dynamic,Eigen::RowMajor> U;
-  igl::readDMAT(argv[2],U);
+  {
+    Eigen::Matrix< double ,Eigen::Dynamic,Eigen::Dynamic,Eigen::RowMajor> Ud;
+    igl::readDMAT(argv[2],Ud);
+    U = Ud.cast<float>();
+  }
   assert((U.rows() == V.rows()*3) && "#U should be 3*#V");
   //std::cout<<"**warning** resizing to min(U.cols(),100)"<<std::endl;
   //U.conservativeResize(U.rows(),std::min(100,(int)U.cols()));
@@ -48,12 +55,14 @@ int main(int argc, char *argv[])
   ///////////////////////////////////////////////////////////////////
   // Initialize viewer and opengl context
   ///////////////////////////////////////////////////////////////////
-  igl::viewer::Viewer v;
-  v.data.set_mesh(V,F);
-  v.data.set_face_based(false);
-  v.core.show_lines = false;
+  igl::opengl::glfw::Viewer v;
+  v.data().set_mesh(V,F);
+  v.data().set_face_based(false);
+  v.data().show_lines = false;
   v.launch_init(true,false);
-  v.opengl.shader_mesh.free();
+  v.data().meshgl.init();
+  igl::opengl::destroy_shader_program(v.data().meshgl.shader_mesh);
+
   
   ///////////////////////////////////////////////////////////////////
   // Compile new shaders
@@ -61,7 +70,6 @@ int main(int argc, char *argv[])
   {
     std::string mesh_vertex_shader_string =
 R"(#version 150
-uniform mat4 model;
 uniform mat4 view;
 uniform mat4 proj;
 in vec3 position;
@@ -96,77 +104,82 @@ void main()
   }
   vec3 deformed = position + displacement;
 
-  position_eye = vec3 (view * model * vec4 (deformed, 1.0));
+  position_eye = vec3 (view * vec4 (deformed, 1.0));
   gl_Position = proj * vec4 (position_eye, 1.0);
   Kai = Ka;
   Kdi = Kd;
   Ksi = Ks;
+  texcoordi = texcoord;
 })";
 
     std::string mesh_fragment_shader_string =
 R"(#version 150
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 proj;
-uniform vec4 fixed_color;
-in vec3 position_eye;
-uniform vec3 light_position_world;
-vec3 Ls = vec3 (1, 1, 1);
-vec3 Ld = vec3 (1, 1, 1);
-vec3 La = vec3 (1, 1, 1);
-in vec4 Ksi;
-in vec4 Kdi;
-in vec4 Kai;
-uniform float specular_exponent;
-uniform float lighting_factor;
-out vec4 outColor;
-void main()
-{
-  vec3 xTangent = dFdx(position_eye);
-  vec3 yTangent = dFdy(position_eye);
-  vec3 normal_eye = normalize( cross( yTangent, xTangent ) );
+  uniform mat4 view;
+  uniform mat4 proj;
+  uniform vec4 fixed_color;
+  in vec3 position_eye;
+  uniform vec3 light_position_eye;
+  vec3 Ls = vec3 (1, 1, 1);
+  vec3 Ld = vec3 (1, 1, 1);
+  vec3 La = vec3 (1, 1, 1);
+  in vec4 Ksi;
+  in vec4 Kdi;
+  in vec4 Kai;
+  in vec2 texcoordi;
+  uniform sampler2D tex;
+  uniform float specular_exponent;
+  uniform float lighting_factor;
+  uniform float texture_factor;
+  uniform float matcap_factor;
+  uniform float double_sided;
+  out vec4 outColor;
+  void main()
+  {
+    vec3 xTangent = dFdx(position_eye);
+    vec3 yTangent = dFdy(position_eye);
+    vec3 normal_eye = normalize( cross( yTangent, xTangent ) );
 
-vec3 Ia = La * vec3(Kai);    // ambient intensity
-vec3 light_position_eye = vec3 (view * vec4 (light_position_world, 1.0));
-vec3 vector_to_light_eye = light_position_eye - position_eye;
-vec3 direction_to_light_eye = normalize (vector_to_light_eye);
-float dot_prod = dot (direction_to_light_eye, normal_eye);
-float clamped_dot_prod = max (dot_prod, 0.0);
-vec3 Id = Ld * vec3(Kdi) * clamped_dot_prod;    // Diffuse intensity
+    if(matcap_factor == 1.0f)
+    {
+      vec2 uv = normalize(normal_eye).xy * 0.5 + 0.5;
+      outColor = texture(tex, uv);
+    }else
+    {
+      vec3 Ia = La * vec3(Kai);    // ambient intensity
 
-vec3 reflection_eye = reflect (-direction_to_light_eye, normal_eye);
-vec3 surface_to_viewer_eye = normalize (-position_eye);
-float dot_prod_specular = dot (reflection_eye, surface_to_viewer_eye);
-dot_prod_specular = float(abs(dot_prod)==dot_prod) * max (dot_prod_specular, 0.0);
-float specular_factor = pow (dot_prod_specular, specular_exponent);
-vec3 Kfi = 0.5*vec3(Ksi);
-vec3 Lf = Ls;
-float fresnel_exponent = 2*specular_exponent;
-float fresnel_factor = 0;
-{
-  float NE = max( 0., dot( normal_eye, surface_to_viewer_eye));
-  fresnel_factor = pow (max(sqrt(1. - NE*NE),0.0), fresnel_exponent);
-}
-vec3 Is = Ls * vec3(Ksi) * specular_factor;    // specular intensity
-vec3 If = Lf * vec3(Kfi) * fresnel_factor;     // fresnel intensity
-vec4 color = vec4(lighting_factor * (If + Is + Id) + Ia + 
-  (1.0-lighting_factor) * vec3(Kdi),(Kai.a+Ksi.a+Kdi.a)/3);
-outColor = color;
-if (fixed_color != vec4(0.0)) outColor = fixed_color;
-})";
-    v.opengl.shader_mesh.init(
+      vec3 vector_to_light_eye = light_position_eye - position_eye;
+      vec3 direction_to_light_eye = normalize (vector_to_light_eye);
+      float dot_prod = dot (direction_to_light_eye, normalize(normal_eye));
+      float clamped_dot_prod = abs(max (dot_prod, -double_sided));
+      vec3 Id = Ld * vec3(Kdi) * clamped_dot_prod;    // Diffuse intensity
+
+      vec3 reflection_eye = reflect (-direction_to_light_eye, normalize(normal_eye));
+      vec3 surface_to_viewer_eye = normalize (-position_eye);
+      float dot_prod_specular = dot (reflection_eye, surface_to_viewer_eye);
+      dot_prod_specular = float(abs(dot_prod)==dot_prod) * abs(max (dot_prod_specular, -double_sided));
+      float specular_factor = pow (dot_prod_specular, specular_exponent);
+      vec3 Is = Ls * vec3(Ksi) * specular_factor;    // specular intensity
+      vec4 color = vec4(lighting_factor * (Is + Id) + Ia + (1.0-lighting_factor) * vec3(Kdi),(Kai.a+Ksi.a+Kdi.a)/3);
+      outColor = mix(vec4(1,1,1,1), texture(tex, texcoordi), texture_factor) * color;
+      if (fixed_color != vec4(0.0)) outColor = fixed_color;
+    }
+  }
+)";
+
+    igl::opengl::create_shader_program(
       mesh_vertex_shader_string,
-      mesh_fragment_shader_string, 
-      "outColor");
+      mesh_fragment_shader_string,
+      {},
+      v.data().meshgl.shader_mesh);
   }
 
   ///////////////////////////////////////////////////////////////////
   // Send texture and vertex attributes to GPU
   ///////////////////////////////////////////////////////////////////
   {
-    GLuint prog_id = v.opengl.shader_mesh.program_shader;
+    GLuint prog_id = v.data().meshgl.shader_mesh;
     glUseProgram(prog_id);
-    GLuint VAO = v.opengl.vao_mesh;
+    GLuint VAO = v.data().meshgl.vao_mesh;
     glBindVertexArray(VAO);
     GLuint IBO;
     glGenBuffers(1, &IBO);
@@ -178,8 +191,8 @@ if (fixed_color != vec4(0.0)) outColor = fixed_color;
     glEnableVertexAttribArray(iid);
     glBindVertexArray(0);
     glActiveTexture(GL_TEXTURE0);
-    //glGenTextures(1, &v.opengl.vbo_tex);
-    glBindTexture(GL_TEXTURE_2D, v.opengl.vbo_tex);
+    //glGenTextures(1, &v.data().meshgl.vbo_tex);
+    glBindTexture(GL_TEXTURE_2D, v.data().meshgl.vbo_tex);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
@@ -196,7 +209,7 @@ if (fixed_color != vec4(0.0)) outColor = fixed_color;
   q0(0) = 1;
   Eigen::VectorXf q1 = Eigen::VectorXf::Zero(m,1);
 
-  v.callback_pre_draw = [&U,&q0,&q1,&m,&n,&s](igl::viewer::Viewer & v) ->bool
+  v.callback_pre_draw = [&U,&q0,&q1,&m,&n,&s](igl::opengl::glfw::Viewer & v) ->bool
   {
     static size_t count = 0;
     const int keyrate = 15;
@@ -212,7 +225,7 @@ if (fixed_color != vec4(0.0)) outColor = fixed_color;
     /////////////////////////////////////////////////////////
     // Send uniforms to shader
     /////////////////////////////////////////////////////////
-    GLuint prog_id = v.opengl.shader_mesh.program_shader;
+    GLuint prog_id = v.data().meshgl.shader_mesh;
     glUseProgram(prog_id);
     GLint n_loc = glGetUniformLocation(prog_id,"n");
     glUniform1i(n_loc,n);
@@ -223,17 +236,21 @@ if (fixed_color != vec4(0.0)) outColor = fixed_color;
     GLint q_loc = glGetUniformLocation(prog_id,"q");
     glUniform1fv(q_loc,U.cols(),qa.data());
     // Do this now so that we can stop texture from being loaded by viewer
-    if (v.data.dirty)
+    if (v.data().dirty)
     {
-      v.opengl.set_data(v.data, v.core.invert_normals);
-      v.data.dirty = igl::viewer::ViewerData::DIRTY_NONE;
+      v.data().updateGL(
+        v.data(), 
+        v.data().invert_normals,
+        v.data().meshgl
+        );
+      v.data().dirty = igl::opengl::MeshGL::DIRTY_NONE;
     }
-    v.opengl.dirty &= ~igl::viewer::ViewerData::DIRTY_TEXTURE;
+    v.data().meshgl.dirty &= ~igl::opengl::MeshGL::DIRTY_TEXTURE;
     return false;
   };
 
-  v.core.animation_max_fps = 60.0;
-  v.core.is_animating = true;
+  v.core().animation_max_fps = 60.0;
+  v.core().is_animating = true;
   v.launch_rendering(true);
   v.launch_shut();
 }
